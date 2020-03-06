17543bff2cb3c57ad9e62bbd9033cb7fec265873
AdityaNPL <adityaniraula@gmail.com>
2020-03-06 16:41:11 +0000

    updated waypoint

--- waypoint2.py
+++ waypoint2.py
@@ -1,8 +1,8 @@
 import os
 import time
 
-for i in range(1,6):
+for i in range(2):
     os.system("rosrun rotors_gazebo waypoint_publisher 0 0 2 1 __ns:=firefly2")
-    os.system("rosrun rotors_gazebo waypoint_publisher 1 0 2 1 __ns:=firefly2")
-    os.system("rosrun rotors_gazebo waypoint_publisher 1 1 2 1 __ns:=firefly2")
-    os.system("rosrun rotors_gazebo waypoint_publisher 0 1 2 1 __ns:=firefly2")
+    os.system("rosrun rotors_gazebo waypoint_publisher 5 0 2 1 __ns:=firefly2")
+    os.system("rosrun rotors_gazebo waypoint_publisher 5 5 2 1 __ns:=firefly2")
+    os.system("rosrun rotors_gazebo waypoint_publisher 0 5 2 1 __ns:=firefly2")
--- waypoint3.py
+++ waypoint3.py
@@ -1,8 +1,8 @@
 import os
 import time
 
-for i in range(1,6):
+for i in range(2):
     os.system("rosrun rotors_gazebo waypoint_publisher 0 0 3 1 __ns:=firefly3")
-    os.system("rosrun rotors_gazebo waypoint_publisher 1 0 3 1 __ns:=firefly3")
-    os.system("rosrun rotors_gazebo waypoint_publisher 1 1 3 1 __ns:=firefly3")
-    os.system("rosrun rotors_gazebo waypoint_publisher 0 1 3 1 __ns:=firefly3")
+    os.system("rosrun rotors_gazebo waypoint_publisher 5 0 3 1 __ns:=firefly3")
+    os.system("rosrun rotors_gazebo waypoint_publisher 5 5 3 1 __ns:=firefly3")
+    os.system("rosrun rotors_gazebo waypoint_publisher 0 5 3 1 __ns:=firefly3")
