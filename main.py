faabe81890a1b41fe03bca7d5828c32d282234ea
AdityaNPL <adityaniraula@gmail.com>
2020-03-06 13:30:47 +0000

    Create main script

---
+++ main.py
@@ -0,0 +1,4 @@
+import os
+
+os.system("source ~/.bashrc")
+os.system("roslaunch rotors_gazebo simulation.launch &")
--- src/rotors_simulator/rotors_gazebo/scripts/listener.py
+++ src/rotors_simulator/rotors_gazebo/scripts/listener.py
@@ -17,6 +17,3 @@ def listener():
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
-
-if __name__ == '__main__':
-    listener()
