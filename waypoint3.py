9231378653b82fd574b0ab48c0957f0b8b022c27
AdityaNPL <adityaniraula@gmail.com>
2020-03-11 23:50:17 +0000

    updated listener

--- waypoint3.py
+++ waypoint3.py
@@ -1,8 +1,13 @@
 import os
 import time
+import sys
+import subprocess
 
-for i in range(2):
-    os.system("rosrun rotors_gazebo waypoint_publisher 0 0 3 1 __ns:=firefly3")
-    os.system("rosrun rotors_gazebo waypoint_publisher 5 0 3 1 __ns:=firefly3")
-    os.system("rosrun rotors_gazebo waypoint_publisher 5 5 3 1 __ns:=firefly3")
-    os.system("rosrun rotors_gazebo waypoint_publisher 0 5 3 1 __ns:=firefly3")
+# for i in range(2):
+#     os.system("rosrun rotors_gazebo waypoint_publisher 0 0 3 1 __ns:=firefly3")
+#     os.system("rosrun rotors_gazebo waypoint_publisher 5 0 3 1 __ns:=firefly3")
+#     os.system("rosrun rotors_gazebo waypoint_publisher 5 5 3 1 __ns:=firefly3")
+#     os.system("rosrun rotors_gazebo waypoint_publisher 0 5 3 1 __ns:=firefly3")
+
+out=subprocess.call(["rosservice call /gazebo/get_model_state '{model_name: firefly3    }'"],shell=True)
+print(out)
