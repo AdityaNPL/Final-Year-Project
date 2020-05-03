a06d079c522ba8e6b38179231771ce4d7b284069
AdityaNPL <adityaniraula@gmail.com>
2020-05-04 00:48:07 +0100

    new test

--- GridWorld/Grid.py
+++ GridWorld/Grid.py
@@ -10,10 +10,10 @@ class GridClass:
     def __init__(self):
         self.grid_ui_obj = GridUI.GridUIClass(self.grid_width, self.grid_height)
 
-        self.adv = Adversary.AdversaryRobotClass(650, 0, self.grid_ui_obj, "adv1")
-        self.ally1 = Ally.AllyRobotClass(0, 350, self.grid_ui_obj, "1")
-        self.ally2 = Ally.AllyRobotClass(1300, 350, self.grid_ui_obj, "2")
-        self.ally3 = Ally.AllyRobotClass(650, 700, self.grid_ui_obj, "3")
+        self.adv = Adversary.AdversaryRobotClass(0, 0, self.grid_ui_obj, "adv1")
+        self.ally1 = Ally.AllyRobotClass(650, 0, self.grid_ui_obj, "1")
+        self.ally2 = Ally.AllyRobotClass(0, 700, self.grid_ui_obj, "2")
+        self.ally3 = Ally.AllyRobotClass(1300, 700, self.grid_ui_obj, "3")
 
         self.adv.set_opponent_details([self.ally1, self.ally2, self.ally3])
         self.ally1.set_opponent_details(self.adv)
